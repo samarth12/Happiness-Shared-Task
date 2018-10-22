eeb505fe3d54598942030b66259e8f06236d97a5
samarth12 <samarth12@live.com>
2018-10-22 09:41:29 -0700

    word2vec and dataset loading

--- data.py
+++ data.py
@@ -3,33 +3,45 @@ import gensim
 import pickle
 from gensim.models import Word2Vec
 
-# sentences = []
-# with open('training_data.csv', 'r') as csvFile:
-#     reader = csv.reader(csvFile)
-#     for row in reader:
-#         #print(row[1])
-#         sentences.append(row[1])
+#Load the dataset sentences
+def load_data(file):
+    sentences = []
+    with open(file, 'r') as csvFile:
+        reader = csv.reader(csvFile)
+        for row in reader:
+            #print(row[1])
+            sentences.append(row[1])
 
-# csvFile.close()
+    csvFile.close()
+    return sentences
 
-# print(len(sentences))
+#Train word2vec model for input sentences
+def train_wv(data):
+    model = gensim.models.Word2Vec(
+            data,
+            size=150,
+            window=10,
+            min_count=2,
+            workers=10)
+    model.train(data, total_examples=len(data), epochs=10)
+    print("Done")
+    return model
 
-# model = gensim.models.Word2Vec(
-#         sentences,
-#         size=150,
-#         window=10,
-#         min_count=2,
-#         workers=10)
-# model.train(sentences, total_examples=len(sentences), epochs=10)
-# print("Done")
 
-#model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)  
-#model.init_sims(replace=True)
-#model.save('google_word')
+#Load the Google News Word2Vec
+def load_google_wv(file):
+    model =  Word2Vec.load_word2vec_format(file, binary=True)  
+    return model
 
-#later load the model
-loaded_model = Word2Vec.load('google_word')
 
-w1 = "friend"
+def main():
 
-print(loaded_model.wv.most_similar(positive = w1))
+
+    load_data('training_data.csv')
+    loaded_model = load_google_wv('GoogleNews-vectors-negative300.bin')
+
+    w1 = "friend"
+    print(loaded_model.wv.most_similar(positive = w1))
+
+
+main()
